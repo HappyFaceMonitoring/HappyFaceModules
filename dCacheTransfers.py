60857b13f9afa32b707d5a4650d0f0d984819c3c
Marcus Schmitt <marcus.schmitt90@gmail.com>
2013-03-01 15:56:54 +0000

    fixed again IndexError for Transfers, removed debugging output from Sam
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFaceModules/trunk@88 59e662cd-a22a-4296-a36a-137269dfb10d

--- dCacheTransfers.py
+++ dCacheTransfers.py
@@ -82,45 +82,46 @@ class dCacheTransfers(hf.module.ModuleBase):
         fobj = open(self.source.getTmpPath(), 'r')
         speed_sum = 0
         for line in fobj:
-            line_split = line.split(' ')
-            appender = {}
-            if len(line_split) <= 12:
+            try:
+                line_split = line.split(' ')
+                appender = {}
+                if line_split[3] == 'GFtp-2':
+                    if 'f01-' in line_split[7]:
+                        keep = str(line_split[9]) + str(line_split[10]) + str(line_split[11])
+                        trash = line_split.pop(9)
+                        trash = line_split.pop(10)
+                        line_split[9] = keep
+                    else:
+                        continue
+                if line_split[11] == 'RUNNING':
+                    appender['protocol'] = line_split[3]
+                    appender['pnfsid'] = line_split[6]
+                    appender['pool'] = line_split[7]
+                    appender['host'] = line_split[8]
+                    appender['status_text'] = line_split[9]
+                    appender['since'] = int(line_split[10])
+                    appender['transferred'] = float(line_split[13]) / 1024.0 / 1024.0 / 1024.0
+                    appender['speed'] = float(line_split[14]) * 1000.0 / 1024.0
+                    data['total_transfers'] += 1
+                    speed_sum += float(line_split[14]) * 1000.0 / 1024.0
+                    if int(appender['speed']) <= self.speed_critical_limit:
+                        appender['status'] = 0.0
+                        data['below_speed_critical_limit'] += 1
+                    elif int(appender['speed']) <= self.speed_warning_limit:
+                        appender['status'] = 0.5
+                        data['below_speed_warning_limit'] += 1
+                    else:
+                        appender['status'] = 1.0
+                        
+                    if appender['since'] >= (self.time_critical_limit * 3600 * 1000) and appender['status'] != 0.0:
+                        data['status'] = 0.0
+                        data['exceed_time_critical_limit'] += 1
+                    elif appender['since'] >= (self.time_warning_limit * 3600 * 1000) and appender['status'] == 1.0:
+                        data['status'] = 0.5
+                        data['exceed_time_warning_limit'] += 1
+                    self.details_db_value_list.append(appender)
+            except IndexError:
                 continue
-            if line_split[3] == 'GFtp-2':
-                if 'f01-' in line_split[7]:
-                    keep = str(line_split[9]) + str(line_split[10]) + str(line_split[11])
-                    trash = line_split.pop(9)
-                    trash = line_split.pop(10)
-                    line_split[9] = keep
-                else:
-                    continue
-            if line_split[11] == 'RUNNING':
-                appender['protocol'] = line_split[3]
-                appender['pnfsid'] = line_split[6]
-                appender['pool'] = line_split[7]
-                appender['host'] = line_split[8]
-                appender['status_text'] = line_split[9]
-                appender['since'] = int(line_split[10])
-                appender['transferred'] = float(line_split[13]) / 1024.0 / 1024.0 / 1024.0
-                appender['speed'] = float(line_split[14]) * 1000.0 / 1024.0
-                data['total_transfers'] += 1
-                speed_sum += float(line_split[14]) * 1000.0 / 1024.0
-                if int(appender['speed']) <= self.speed_critical_limit:
-                    appender['status'] = 0.0
-                    data['below_speed_critical_limit'] += 1
-                elif int(appender['speed']) <= self.speed_warning_limit:
-                    appender['status'] = 0.5
-                    data['below_speed_warning_limit'] += 1
-                else:
-                    appender['status'] = 1.0
-                    
-                if appender['since'] >= (self.time_critical_limit * 3600 * 1000) and appender['status'] != 0.0:
-                    data['status'] = 0.0
-                    data['exceed_time_critical_limit'] += 1
-                elif appender['since'] >= (self.time_warning_limit * 3600 * 1000) and appender['status'] == 1.0:
-                    data['status'] = 0.5
-                    data['exceed_time_warning_limit'] += 1
-                self.details_db_value_list.append(appender)
                 
         data['warning_transfers'] = data['below_speed_warning_limit'] + data['exceed_time_warning_limit']
         data['critical_transfers'] = data['below_speed_critical_limit'] + data['exceed_time_critical_limit']
