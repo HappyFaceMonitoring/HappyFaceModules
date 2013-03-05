942847523a7e6e4482db1ceba54e581b95425e65
Marcus Schmitt <marcus.schmitt90@gmail.com>
2013-03-05 17:52:16 +0000

    Improved Summary module, more flexibility for different sites
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFaceModules/trunk@94 59e662cd-a22a-4296-a36a-137269dfb10d

--- Summary.py
+++ Summary.py
@@ -21,11 +21,13 @@ from string import strip
 
 class Summary(hf.module.ModuleBase):
     config_keys = {
-        'site_keys': ('Colon separated list of site names.\nThe name corresponds to a config entry with the actual URL', ''),
+        'site_keys': ('Colon separated list of site names. The values are prefixes for the actual url and category configuration. For example use desy', 'desy'),
         'categories': ('Colon separated list of HF categories to display', ''),
-        'SITE_KEY': ('URL of a HappyFace XML output.\nReplace SITE_KEY by whatever you specified in site_keys', ''),
+        'SITE_KEY': ('URL of a HappyFace XML output. Replace SITE_KEY with a key from site_keys like desy_site', ''),
+        'SITE_CAT': ('title of categories to be parsed, replace SITE_CAT with a key from site_keys like desy_cat',''),
+        'pic_path': ('path to local folder with the images for hf3.','/HappyFace/gridka/static/themes/armin_box_arrows/')
     }
-    config_hint = ''
+    config_hint = 'The big problem is that different sites use different titles for their categories, so its necessary to define a list of categories for each site, make sure to use the same order for all sites!'
     
     table_columns = [], []   
 
@@ -42,23 +44,18 @@ class Summary(hf.module.ModuleBase):
     def prepareAcquisition(self):
 
         # definition of the database table keys and pre-defined values
-        try:
-            self.site_keys = map(strip, self.config['site_keys'].split(','))
+            self.site_keys = map(strip, self.config['site_keys'].lower().split(','))
+            self.order = map(strip, self.config['site_keys'].split(','))
             self.sites = []
-            self.source = ''
+            self.cats = []
             self.details_db_value_list = []
             for i,site in enumerate(self.site_keys):
-                self.sites.append(hf.downloadService.addDownload(self.config[self.site_keys[i]]))
-                self.source += str(self.site_keys[i]) + ': ' + str(self.sites[i].getSourceUrl()) + '\n'
-            self.categories = self.config['categories'].split(',')
-        except KeyError, ex:
-            raise hf.exceptions.ConfigError('Required parameter "%s" not specified' % str(e))
+                self.sites.append(hf.downloadService.addDownload(self.config[str(self.site_keys[i]) + '_site']))
+                self.cats.append(map(strip, self.config[str(self.site_keys[i]) + '_cat'].split(',')))
         
     def extractData(self):
         data = {}
-        data['source_url'] = self.source
         data['status'] = 1
-        count = 0
         for i,source_key in enumerate(self.sites):
             source = etree.parse(open(source_key.getTmpPath()))
             root = source.getroot()
@@ -77,10 +74,12 @@ class Summary(hf.module.ModuleBase):
                             details_db_value['catlink'] = cat_item.text
                         if cat_item.tag == 'status':
                             details_db_value['status'] = float(cat_item.text)
-                    if details_db_value['cattitle'] in self.categories:
-                        details_db_value['order'] = int(count)
-                        self.details_db_value_list.append(details_db_value)
-                        count = count + 1
+                        
+                    for j,allowed in enumerate(self.cats[i]):
+                        if allowed == details_db_value['cattitle']:
+                            details_db_value['order'] = 1000*i + j
+                            self.details_db_value_list.append(details_db_value)
+        
         return data
             
     def fillSubtables(self, parent_id):
@@ -89,35 +88,26 @@ class Summary(hf.module.ModuleBase):
     def getTemplateData(self):
         data = hf.module.ModuleBase.getTemplateData(self)
         details_list = self.subtables['details'].select().where(self.subtables['details'].c.parent_id==self.dataset['id']).order_by(self.subtables['details'].c.order.asc()).execute().fetchall()
-        rawhelpdata = map(dict, details_list)
-        helpdata = {}
-        tempsite = ''
-        key_order = []
-        count = -1
-        
-        for i,cat in enumerate(rawhelpdata):
-            if cat['site'] == tempsite:
-                helpdata['%02i' %count].append(cat)
+        details_list = map(dict, details_list)
+        pic_path = self.config['pic_path']
+        sorted_list = [[]]
+        last = 0
+        for i,cats in enumerate(details_list):
+            act = int(cats['order']/1000)
+            if cats['status'] >= 0.66:
+                cats['status'] = pic_path + "mod_happy.png" 
+            elif cats['status'] >= 0.33:
+                cats['status'] = pic_path + "mod_neutral.png" 
             else:
-                count += 1
-                helpdata['%02i' %count] = []
-                tempsite = cat['site']
-                helpdata['%02i' %count].append(cat)
-        for i,keys in enumerate(helpdata['00']):
-            key_order.append(keys['cattitle'])
-            
-        for c,rows in helpdata.iteritems():
-            appender = []
-            for i,keys in enumerate(key_order):
-                for z,cols in enumerate(rows):
-                    if cols['cattitle'] == keys:
-                        if cols['status'] >= 0.66:
-                           cols['status'] = "/static/themes/armin_box_arrows/mod_happy.png" 
-                        elif cols['status'] >= 0.33:
-                           cols['status'] = "/static/themes/armin_box_arrows/mod_neutral.png" 
-                        else:
-                           cols['status'] = "/static/themes/armin_box_arrows/mod_unhappy.png" 
-                        appender.append(cols)
-            data['_' + c] = appender
+                cats['status'] = pic_path + "mod_unhappy.png"
+            if act != last:
+                last += 1
+                sorted_list.append([])
+                sorted_list[last].append(cats)
+            else:
+                sorted_list[last].append(cats)
+        data['details'] = sorted_list
+        data['main_names'] = map(strip, self.config['site_keys'].split(','))
+        data['cat_header'] = map(strip, self.config['categories'].split(','))
         return data
                         
