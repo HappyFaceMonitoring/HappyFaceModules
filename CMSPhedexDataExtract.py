beb2ec6bdad7495f4835043d3613c879ab2baec7
Marcus Schmitt <marcus.schmitt90@gmail.com>
2013-03-12 17:17:17 +0000

    Rewritten module code and new method for displaying data
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFaceModules/trunk@101 59e662cd-a22a-4296-a36a-137269dfb10d

--- CMSPhedexDataExtract.py
+++ CMSPhedexDataExtract.py
@@ -48,7 +48,7 @@ class CMSPhedexDataExtract(hf.module.ModuleBase):
             self.blacklist = self.config['blacklist']
         except AttributeError:
             self.blacklist = []
-        self.time = int(time.time())
+        self.time = int(time.time())/3600*3600
         self.url += str(self.time-self.time_range*3600)
         self.source = hf.downloadService.addDownload(self.url)
         self.details_db_value_list = []
@@ -59,137 +59,74 @@ class CMSPhedexDataExtract(hf.module.ModuleBase):
         my_cmap = plt.get_cmap('RdYlGn')
         my_cmap.set_over('w')
         data = {'direction' : self.link_direction, 'source_url' : self.source.getSourceUrl(), 'time_range' : self.time_range, 'request_timestamp' : self.time}
-        #prepare everything
-        y = []
-        x = []
-        weight = []
-        ykey = []
-        x0 = self.time/3600*3600-self.time_range*3600
-        height = 0
-        xpos = []
-        for i in range(self.time_range / 6):
-            xpos.append(i * 6)
-        xpos.append(self.time_range)
-        xkey = []
-        h0 = int(24*(self.time/3600.0/24.0 - self.time/3600/24)) + 6
-        for i in enumerate(xpos):
-            h0 -= 6
-            if h0 < 0:
-                h0 += 24
-            xkey.append('%2i:00' %h0)
-        xkey.reverse()
-        # parse data
+        
+        x_line = self.time - 7200 #data with a timestamp greater than this one will be used for status evaluation
+        #store the last qualities of the Tx links within those lists and evaluate them
+        T0_link_list = []
+        T1_link_list = []
+        T2_link_list = []
+        T3_link_list = []
         fobj = json.load(open(self.source.getTmpPath(), 'r'))['phedex']['link']
-        for i,links in enumerate(fobj):
+        
+        for links in fobj:
             if links[self.link_direction] == self.your_name and links[self.parse_direction] not in self.blacklist:
                 link_name = links[self.parse_direction]
-                ykey.append(link_name)
-                for j,transfer in enumerate(links['transfer']):
+                for transfer in links['transfer']:
                     help_append = {}
-                    x.append(int(transfer['timebin']-x0)/3600)
-                    y.append(height)
                     help_append['timebin'] = int(transfer['timebin'])
                     help_append['done_files'] = done = int(transfer['done_files'])
                     help_append['fail_files'] = fail = int(transfer['fail_files'])
                     help_append['rate'] = int(transfer['rate'])
                     help_append['name'] = link_name
                     if done != 0:
-                        weight.append(float(done)/float(done + fail))
                         help_append['quality'] = float(done)/float(done + fail)
+                        help_append['color'] = mcol.rgb2hex(my_cmap(help_append['quality']))
+                        self.details_db_value_list.append(help_append)
+                        
+                        if help_append['timebin'] >= x_line:
+                            if 'T0' in link_name:
+                                T0_link_list.append(help_append['quality'])
+                            elif 'T1' in link_name:
+                                T1_link_list.append(help_append['quality'])
+                            elif 'T2' in link_name:
+                                T2_link_list.append(help_append['quality'])
+                            elif 'T3' in link_name:
+                                T3_link_list.append(help_append['quality'])
+                                
                     elif fail != 0:
-                        weight.append(0.0)
                         help_append['quality'] = 0.0
-                    else:
-                        weight.append(1.5)
-                        help_append['quality'] = 1.5
-                    help_append['color'] = mcol.rgb2hex(my_cmap(help_append['quality']))
-                    self.details_db_value_list.append(help_append)
-                height += 1
-        if len(y) > 0: 
-            H = []
-            for i in range(0,height):
-                H.append([])
-                for j in range(0,self.time_range + 1):
-                    H[i].append(1.5)
-            for i,group in enumerate(x):
-                H[y[i]][group] = weight[i]
-            T1_data = []
-            T2_data = []
-            for i,name  in enumerate(ykey):
-                if 'T1' in name:
-                    T1_data.append(H[i])
-                elif 'T2' in name:
-                    T2_data.append(H[i])
-            
-            for i in range(3):
-                bad1 = 0
-                sum1 = 0
-                sum2 = 0
-                bad2 = 0
-                for j, link in enumerate(T1_data):
-                    if  link[self.time_range - j] <= 0.5:
-                        bad1 += 1
-                        sum1 += 1
-                    elif link[self.time_range - j] != 1.5:
-                        sum1 += 1
-                for j, link in enumerate(T2_data):
-                    if link[self.time_range - j] <+ 0.5:
-                        bad2 += 1
-                        sum2 += 1
-                    elif link[self.time_range - j] != 1.5:
-                        sum2 += 1
-
-            data['status'] = 1.0
-            try:
-                if float(bad1) / sum1 >= 0.5 or float(bad2) / sum2 >= 0.5:
-                    data['status'] = 0.0
-            except ZeroDivisionError:
-                if sum1 == 0 and sum2 != 0:
-                    if float(bad2) / sum2 >= 0.5:
-                        data['status'] = 0.0
-                elif sum1 != 0 and sum2 == 0:
-                    if float(bad1) / sum1 >= 0.5:
-                        data['status'] = 0.0
-        else:
-            data['status'] = -1
-            data['error_string'] = 'No data returned by DB query!'
+                        help_append['color'] = mcol.rgb2hex(my_cmap(help_append['quality']))
+                        self.details_db_value_list.append(help_append)
+                        
+                        if help_append['timebin'] >= x_line:
+                            if 'T0' in link_name:
+                                T0_link_list.append(help_append['quality'])
+                            elif 'T1' in link_name:
+                                T1_link_list.append(help_append['quality'])
+                            elif 'T2' in link_name:
+                                T2_link_list.append(help_append['quality'])
+                            elif 'T3' in link_name:
+                                T3_link_list.append(help_append['quality'])
+       # code for status evaluation TODO
+       #for i,eval_list in enumerate([T0_link_list, T1_link_list, T2_link_list, T3_link_list]):
+            #good_link = 0
+            #bad_link = 0
+            #for quality in eval_list:
+                #if quality < 0.5:
+                    #bad_link += 1
+                #else:
+                    #good_link += 1
+            #if 
         return data
-    
-    #def plot_func(self, H, xbin, ybin, y_key, x_pos, x_key):
-        #import matplotlib.colors as mcol
-        #import matplotlib.pyplot as plt
-        #import numpy as np
-        #if ybin < 8:
-            #ysize = 8
-        #else:
-            #ysize = ybin
-        #fig = plt.figure(figsize=(16,ysize))
-        #H = []
-        #for i in range(0,ybin):
-            #H.append([])
-            #for j in range(0,xbin):
-                #H[i].append(0.0)
-        #for i,group in enumerate(x):
-            #H[y[i]][group] = w[i]
-        #H = np.array(H)
-        #extent = [0, xbin, 0, ybin]
-        #my_cmap = plt.get_cmap('RdYlGn')
-        #my_cmap.set_over('w')
-        #norm = mcol.Normalize(0.0, 1)
-        #plt.imshow(H, extent = extent, interpolation = 'nearest', cmap = my_cmap, norm=norm, aspect='auto', rasterized=True, origin = 'lower')
-        #plt.colorbar(orientation = 'horizontal', fraction = 0.15, shrink = 0.8)
-        #plt.grid(linewidth=1.2, which = 'both')
-        #plt.yticks(np.arange(0, ybin) + 0.5, y_key, size = 'medium')
-        #plt.xticks(x_pos, x_key, size = 'medium', rotation = 30)
-        #fig.savefig(hf.downloadService.getArchivePath(self.run, self.instance_name + '_phedex_' + self.link_direction + self.your_name + '_quality.png'), dpi=60)
-        #return(self.instance_name + '_phedex_' + self.link_direction + self.your_name + '_quality.png')
-    
 
     def fillSubtables(self, parent_id):
         self.subtables['details'].insert().execute([dict(parent_id=parent_id, **row) for row in self.details_db_value_list])
+    
     def getTemplateData(self):
+        
         report_base = strip(self.config['report_base'])
         your_direction = strip(self.config['link_direction'])
+        
         if your_direction == 'from':
             their_direction = 'tofilter'
             your_direction = 'fromfilter'
@@ -197,47 +134,30 @@ class CMSPhedexDataExtract(hf.module.ModuleBase):
             their_direction = 'fromfilter'
             your_direction = 'tofilter'
             
-        #get data from database
         data = hf.module.ModuleBase.getTemplateData(self)
         details_list = self.subtables['details'].select().where(self.subtables['details'].c.parent_id==self.dataset['id']).order_by(self.subtables['details'].c.name.asc()).execute().fetchall()
-        #build a 2d histogram and fill it with transfer quality
-        H = []
-        x = []
-        y = []
-        w = []
-        fails = []
-        done = []
-        rates = []
-        times = []
-        color = []
-        x0 = self.dataset['request_timestamp'] / 3600 * 3600 - self.dataset['time_range'] * 3600
-        y_value_map = {}
-        for i,values in enumerate(details_list):
-            if values['name'] not in y_value_map:
+        
+        raw_data_list = [] #contains dicts {x,y,weight,fails,done,rate,time,color,link} where the weight determines the the color
+        
+        x0 = self.dataset['request_timestamp'] / 3600 * 3600 - self.dataset['time_range'] * 3600 #normalize the timestamps to the requested timerange
+        y_value_map = {} # maps the name of a link to a y-value
+        
+        for values in details_list:
+            if values['name'] not in y_value_map: #add a new entry if the link name is not in the value_map 
                 y_value_map[values['name']] = len(y_value_map)
-            x.append(int(values['timebin']-x0)/3600)
-            times.append(datetime.datetime.fromtimestamp(values['timebin']))
-            y.append(y_value_map[values['name']])
-            fails.append(int(values['fail_files']))
-            done.append(int(values['done_files']))
-            rates.append(float(values['rate'])/1024/1024)
-            w.append(values['quality'])
-            color.append(values['color'])
-        H = []
+            help_dict = {'x':int(values['timebin']-x0)/3600, 'y':int(y_value_map[values['name']]), 'w':str('%.2f' %values['quality']), 'fails':int(values['fail_files']), 'done':int(values['done_files']), 'rate':str('%.3f' %(float(values['rate'])/1024/1024)), 'time':datetime.datetime.fromtimestamp(values['timebin']), 'color':values['color'], 'link':report_base + their_direction + values['name']}
+            raw_data_list.append(help_dict)
+        
         name_mapper = []
+        
         for i in range(len(y_value_map)):
-            H.append([])
             name_mapper.append('-')
-            for j in range(self.dataset['time_range'] + 1):
-                    H[i].append({'w':'', 'c':'#FFFFFF', 'fails': '', 'done':'', 'rates':'', 'time':'', 'link': ''})
+        
         for key in y_value_map.iterkeys():
             name_mapper[y_value_map[key]] = key
-        for i,group in enumerate(x):
-            if w[i] != 1.5:
-                H[y[i]][group] = {'w':str('%.2f' %w[i]), 'fails':fails[i], 'done':done[i], 'rates':str('%.3f' %rates[i]), 'c':color[i], 'time':times[i], 'link':report_base + their_direction + name_mapper[y[i]]}
-            else:
-                H[y[i]][group] = {'w':'', 'c':'#FFFFFF', 'fails': '', 'done':'', 'rates':'', 'time':'', 'link': ''}
-        #iterate over histogram H and fill each bin with an dictionary
-        data['histogram'] = H
+        
+        data['link_list'] = raw_data_list
         data['names'] = name_mapper
+        data['height'] = len(y_value_map) * 15 + 100
+        data['width'] = self.dataset['time_range'] * 15 + 250
         return data
