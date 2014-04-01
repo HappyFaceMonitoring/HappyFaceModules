3264e5906b9caab9ed5379c77b0eb82466abbf0f
Stefan Letzelter <letz.elder@gmail.com>
2014-04-01 09:39:39 +0000

    added HappyHealth module
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/HappyFaceModules/trunk@153 59e662cd-a22a-4296-a36a-137269dfb10d

---
+++ HappyHealth.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 Institut für Experimentelle Kernphysik - Karlsruher Institut für Technologie
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import hf, time, os
+from sqlalchemy import *
+
+#TODO: Name: HappyHealth...  Konvention Name der class gross!!!
+
+class HappyHealth(hf.module.ModuleBase):
+    config_keys = {
+        'directory': ('Path of the directory to show its space', ''),
+        'space_stat_warn': ('Percentage of free space until status is OK', ''),
+        'space_stat_crit': ('Percentage of free space when status is critical', ''),
+        'load0_stat_warn': ('load until status is OK', ''),
+        'load0_stat_crit': ('load when status is critical', ''),
+        'load1_stat_warn': ('load until status is OK', ''),
+        'load1_stat_crit': ('load when status is critical', ''),
+        'load2_stat_warn': ('load until status is OK', ''),
+        'load2_stat_crit': ('load when status is critical', ''),
+    }
+    config_hint = ''
+
+    table_columns = [
+        Column("space_total", INT),
+        Column("space_used", INT),
+        Column("space_free", INT),
+        Column("avg_load_last_1min", FLOAT),
+        Column("avg_load_last_5min", FLOAT),
+        Column("avg_load_last_15min", FLOAT),
+        Column("status", FLOAT)
+    ], []
+
+    def getTemplateData(self):
+        data =  hf.module.ModuleBase.getTemplateData(self)
+        for option in self.config_keys:
+            data["dataset"][option] = self.config[option]
+        return data
+
+
+    def prepareAcquisition(self):
+
+        if 'directory' not in self.config: raise hf.exceptions.ConfigError('directory option not set')
+        if 'space_stat_warn' not in self.config: raise hf.exceptions.ConfigError('space_stat_warn option not set')
+        if 'space_stat_crit' not in self.config: raise hf.exceptions.ConfigError('space_stat_crit option not set')
+        if 'load0_stat_warn' not in self.config: raise hf.exceptions.ConfigError('load0_stat_warn option not set')
+        if 'load0_stat_crit' not in self.config: raise hf.exceptions.ConfigError('load0_stat_crit option not set')
+        if 'load1_stat_warn' not in self.config: raise hf.exceptions.ConfigError('load1_stat_warn option not set')
+        if 'load1_stat_crit' not in self.config: raise hf.exceptions.ConfigError('load1_stat_crit option not set')
+        if 'load2_stat_warn' not in self.config: raise hf.exceptions.ConfigError('load2_stat_warn option not set')
+        if 'load2_stat_crit' not in self.config: raise hf.exceptions.ConfigError('load2_stat_crit option not set')
+
+        self.source_url = 'local'
+        self.path = self.config["directory"]
+        self.sp_limit1 = self.config["space_stat_warn"]
+        self.sp_limit2 = self.config["space_stat_crit"]
+        self.load_limit_warn = [ self.config["load0_stat_warn"] ]
+        self.load_limit_crit = [ self.config["load0_stat_crit"] ]
+        self.load_limit_warn.append( self.config["load1_stat_warn"] )
+        self.load_limit_crit.append( self.config["load1_stat_crit"] )
+        self.load_limit_warn.append( self.config["load2_stat_warn"] )
+        self.load_limit_crit.append( self.config["load2_stat_crit"] )
+
+    def extractData(self):
+
+        st = os.statvfs(self.path)
+        load_status = [0,1,2]
+        totalspace = st.f_blocks*st.f_frsize
+        usedspace = (st.f_blocks-st.f_bavail)*st.f_frsize
+        freespace = st.f_bavail*st.f_frsize
+        freespace_percentage = 100.0*float(freespace)/float(totalspace)
+        load = os.getloadavg()
+        data = {
+        }
+        data.update({
+            "space_total": totalspace,
+            "space_used": usedspace,
+            "space_free": freespace,
+            "avg_load_last_1min": load[0],
+            "avg_load_last_5min": load[1],
+            "avg_load_last_15min": load[2]
+        })
+
+        if freespace_percentage < self.sp_limit2:
+            sp_status = 0.0
+        elif freespace_percentage < self.sp_limit1:
+            sp_status = 0.5
+        else:
+            sp_status = 1.0	
+
+        for i in range(3):
+            if load[i] > self.load_limit_crit[i]:
+                load_status[i] = 0.0
+            elif load[i] > self.load_limit_warn[i]:
+                load_status[i] = 0.5
+            else:
+                load_status[i] = 1.0
+
+        data["status"] = (load_status[0]+load_status[1]+load_status[2]+sp_status)/4.0	
+
+        return data
