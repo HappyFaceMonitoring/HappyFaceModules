5527250356f7b73adfccd2a8b658bc13dc2879e7
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2016-06-30 20:35:25 +0200

    dCacheTransfers.py: Clean up of formal errors pointed out by landscape.io, continued

--- dCacheTransfers.py
+++ dCacheTransfers.py
@@ -14,7 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import hf, logging, datetime
+import hf
 from sqlalchemy import Column, TEXT, INT, FLOAT
 from lxml.html import parse
 from string import strip
