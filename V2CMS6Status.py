105b9ce1a6ada7d1056c9f6ef7e46fad5045b169
harrypuuter <brommer.sebastian@gmail.com>
2016-07-27 11:30:31 +0200

    error status to warning

--- V2CMS6Status.py
+++ V2CMS6Status.py
@@ -409,12 +409,12 @@ class V2CMS6Status(hf.module.ModuleBase):
                     str(temp) + " jobs are longer than " + \
                     str(self.qtime_max) + " hours in the queue. <br>"
             if eff_count < self.min_efficiency and data['running_jobs'] != 0:
-                data['status'] = 0
+                data['status'] = 0.5
                 data['error_msg'] = data['error_msg'] + \
                     " The efficiency is below " + str(self.min_efficiency) + ".<br>"
             try:
                 if float(status_list.count(2)) / float(status_list.count(1)) < self.running_idle_ratio:
-                    data['status'] = 0
+                    data['status'] = 0.5
                     data['error_msg'] = data['error_msg'] + \
                         "The ratio between queued and running jobs is below " + \
                         str(self.running_idle_ratio) + ". <br>"
