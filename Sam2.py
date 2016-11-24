5eb4391c443c118258c3abd618d2290c8bcc993c
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2016-11-24 16:13:59 +0100

    Sam2.py: clean up of formal errors (landscape.io)

--- Sam2.py
+++ Sam2.py
@@ -14,31 +14,53 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import hf, lxml, logging, datetime
-from sqlalchemy import *
-from lxml import etree
+import hf
+from sqlalchemy import TEXT, Column
 import json
 from string import strip
 
 
 class Sam2(hf.module.ModuleBase):
 
+    shwsw_help = ("if amount of warnings is equal or "
+        "greater than this threshold, "
+        "service and happyface status "
+        "will change to warning")
+    shcsw_help = ("if amount of warnings is equal or "
+        "greater than this threshold, "
+        "service and happyface status "
+        "will change to critical")
+    shwse_help = ("if amount of errors is equal or "
+        "greater than this threshold, "
+        "service and happyface status "
+        "will change to warning")
+    shcse_help = ("if amount of errors is equal or "
+        "greater than this threshold, "
+        "service and happyface status "
+        "will change to critical")
+
     ##rebuild those config keys for the new module!
     config_keys = {
         'source_url': ('source url', 'both||url'),
-        'report_url': ('URL for detailed information in dashboard', 'http://dashb-cms-sum.cern.ch/dashboard/request.py/getMetricResultDetails'),
-        'blacklist': ('Colon separated group to exclude from the output, recommended list is for KIT', 'org.cms.glexec.WN-gLExec,org.cms.WN-swinst,org.cms.WN-xrootd-fallback,org.cms.WN-xrootd-access'),
+        'report_url': ('URL for detailed information in dashboard', \
+            'http://dashb-cms-sum.cern.ch/dashboard/request.py/getMetricResultDetails'),
+        'blacklist': ('Colon separated group to exclude from the output, recommended list is for KIT', \
+            'org.cms.glexec.WN-gLExec,org.cms.WN-swinst,org.cms.WN-xrootd-fallback,org.cms.WN-xrootd-access'),
         'service_flavour': ('colon seperated list of flavours to be extracted', 'CREAM-CE,SRMv2'),
         'service_type': ('colon seperated list of types to be extracted', 'CREAM-CE,SRMv2'),
         'SERVICE_hf_warning_sam_min_jobs': ('less tests will result in status warning for this service and the module', '3'),
         'SERVICE_hf_critical_sam_min_jobs': ('less tests will result in status critical for this service and the module', '1'),
-        'SERVICE_hf_warning_sam_warnings': ('if amount of warnings is euqal or greater than this threshold, service and happyface status will change to warning', '2'),
-        'SERVICE_hf_critical_sam_warnings': ('if amount of warnings is equal or greater than this threshold, service and happyface status will change to critical', '4'),
-        'SERVICE_hf_warning_sam_errors': ('if amount of errors is equal or grater than this threshold, service and happyface status will change to warning', '1'),
-        'SERVICE_hf_critical_sam_errors': ('if amount of errors is equal or greater than this threshold, service and happyface will change to status critical', '2'),
+        'SERVICE_hf_warning_sam_warnings': (shwsw_help, '2'),
+        'SERVICE_hf_critical_sam_warnings': (shcsw_help, '4'),
+        'SERVICE_hf_warning_sam_errors': (shwse_help, '1'),
+        'SERVICE_hf_critical_sam_errors': (shcse_help, '2'),
         'ce_blacklist': ('Colon separated group of CEs to exclude from the output', 'None')
     }
-    config_hint = "Due to flexibility reasons you can configure crit and warn thresholds for each service, therefor replace SERVICE with service_type, use _ instead of - and all letters in lowercase"
+    config_hint = ("Due to flexibility reasons "
+        "you can configure crit and warn thresholds for each service, "
+        "therefore replace SERVICE with service_type, "
+        "use _ instead of - and all letters in lowercase"
+        )
 
     table_columns = [], []
 
@@ -75,7 +97,7 @@ class Sam2(hf.module.ModuleBase):
             self.service_error_errors[stype] = int(self.config[str(service) + '_hf_critical_sam_errors'])
 
         self.blacklist = map(strip, self.config['blacklist'].split(','))
-	self.ce_blacklist = map(strip, self.config['ce_blacklist'].split(','))
+        self.ce_blacklist = map(strip, self.config['ce_blacklist'].split(','))
         ## add download tyo queue
         self.source = hf.downloadService.addDownload(self.config['source_url'])
         self.source_url = self.source.getSourceUrl()
@@ -93,7 +115,7 @@ class Sam2(hf.module.ModuleBase):
         services = services['data']['results'][0]['flavours']
 
         ##parse your group and get all the tests
-        for i,service in enumerate(services):
+        for service in services:
             if service['flavourname'] in self.service_flavour and service['servicename'] in self.service_type:
                 for host in service['hosts']:
                     service_host = host['hostname']
@@ -102,7 +124,8 @@ class Sam2(hf.module.ModuleBase):
                     warnings = 0
                     errors = 0
                     tests = 0
-                    for j,test in enumerate(host['metric']):
+                    test = None
+                    for test in host['metric']:
                         status_str = str(test['status']).lower()
                         if test['metric_name'] not in self.blacklist:
                             if str(status_str) == 'warning':
@@ -110,17 +133,27 @@ class Sam2(hf.module.ModuleBase):
                             elif str(status_str) == 'critical':
                                 errors += 1
                             tests += 1
-                        self.details_db_value_list.append({'type':service_type, 'hostName':service_host, 'timeStamp':test['timestamp'], 'metric':test['metric_name'], 'status':status_str})
-                    self.details_db_value_list.append({'type':service_type, 'hostName':service_host, 'timeStamp': '', 'metric':'summary_%s' % test['metric_name'], 'status':host_status})
+                        self.details_db_value_list.append({'type':service_type, \
+                            'hostName':service_host, 'timeStamp':test['timestamp'], \
+                            'metric':test['metric_name'], 'status':status_str} \
+                            )
+                    self.details_db_value_list.append({'type':service_type, \
+                        'hostName':service_host, 'timeStamp': '', \
+                        'metric':'summary_%s' % test['metric_name'], \
+                        'status':host_status})
                     if service_host in self.ce_blacklist:
                         continue
-                    if tests < self.service_error_min_jobs[service_type] or errors >= self.service_error_errors[service_type] or warnings >= self.service_error_warnings[service_type]:
+                    if tests < self.service_error_min_jobs[service_type] or \
+                        errors >= self.service_error_errors[service_type] or \
+                        warnings >= self.service_error_warnings[service_type]:
                         help_stati.append('critical')
-                    elif tests < self.service_warning_min_jobs[service_type] or errors >= self.service_warning_errors[service_type] or warnings >= self.service_warning_warnings[service_type]:
+                    elif tests < self.service_warning_min_jobs[service_type] or \
+                        errors >= self.service_warning_errors[service_type] or \
+                        warnings >= self.service_warning_warnings[service_type]:
                         help_stati.append('warning')
                     else:
                         help_stati.append('ok')
-        ##parsing the file is done, now evaluate everything
+        ##parsing of the file is done, now evaluate everything
         if 'critical' in help_stati:
             data['status'] = 0
         elif 'warning' in help_stati:
@@ -167,7 +200,7 @@ class Sam2(hf.module.ModuleBase):
             .order_by(self.subtables['details'].c.hostName.asc()).execute().fetchall()
         ## sort data and seperate into blacklisted test, critical/warning tests and ok test and build a summary!
 
-        for i,test in enumerate(map(dict, details_list)):
+        for test in map(dict, details_list):
             test['metricfqan'] = test['metric'].replace(' ', '%20').replace('/', '_')
             if test['hostName'] not in hosts and str(test['metric'][0:7]) != 'summary':
                 hosts[test['hostName']]={'ok': 0, 'warn':0, 'status':'ok', 'sum':0, 'crit':0, 'type':test['type']}
@@ -189,11 +222,15 @@ class Sam2(hf.module.ModuleBase):
                 hosts[test['hostName']]['ok'] += 1
                 hosts[test['hostName']]['sum'] += 1
 
-        for key,host in hosts.iteritems():
+        for host in hosts.itervalues():
             service_type = host['type']
-            if host['sum'] < self.service_error_min_jobs[service_type] or host['crit'] >= self.service_error_errors[service_type] or host['warn'] >= self.service_error_warnings[service_type]:
+            if host['sum'] < self.service_error_min_jobs[service_type] or \
+                host['crit'] >= self.service_error_errors[service_type] or \
+                host['warn'] >= self.service_error_warnings[service_type]:
                 host['status'] = 'critical'
-            elif host['sum'] < self.service_warning_min_jobs[service_type] or host['crit'] >= self.service_warning_errors[service_type] or host['warn'] >= self.service_warning_warnings[service_type]:
+            elif host['sum'] < self.service_warning_min_jobs[service_type] or \
+                host['crit'] >= self.service_warning_errors[service_type] or \
+                host['warn'] >= self.service_warning_warnings[service_type]:
                 host['status'] = 'warning'
             else:
                 host['status'] = 'ok'
