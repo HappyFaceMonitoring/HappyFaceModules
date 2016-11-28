3a9054569f52ee278679845511db575d801856fd
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2016-11-28 18:53:20 +0100

    RSSFeed.py: preparation for improvements; clean up (landscape.io)

--- RSSFeed.py
+++ RSSFeed.py
@@ -1,6 +1,6 @@
-import hf, logging
-from sqlalchemy import *
-from datetime import datetime,timedelta
+import hf
+from sqlalchemy import TEXT, INT, Column
+from datetime import timedelta
 from time import mktime,time
 import modules.feedparser
 
@@ -8,7 +8,7 @@ class RSSFeed(hf.module.ModuleBase):
     config_keys = {
         'source': ('URL of the RSS feed', ''),
         'entries': ('Number of entries ("-1" for all entries)', '-1'),
-        'days': ('Show only entries from the last n days ("-1" for all entries)', '7'),
+        'days': ('Show only entries from the last n days ("-1" for all days)', '7'),
         'hide_feed_title': ('Hide feed title', '0')
     }
     config_hint = ''
@@ -33,19 +33,20 @@ class RSSFeed(hf.module.ModuleBase):
         self.source_url = self.source.getSourceUrl()
         try:
             self.entries = self.config['entries']
-        except:
-            raise hf.exceptions.ConfigError('entries option not set!')
+        except Exception:
             self.entries = -1
+            raise hf.exceptions.ConfigError('entries option not set!')
         try:
             self.days = self.config['days']
-        except:
-            raise hf.exceptions.ConfigError('days option not set!')
+        except Exception:
             self.days = -1
+            raise hf.exceptions.ConfigError('days option not set!')
         try:
             self.hide_title = self.config['hide_feed_title']
-        except:
-            raise hf.exceptions.ConfigError('hide_feed_title option not set!')
+        except Exception:
             self.hide_title = 0
+            raise hf.exceptions.ConfigError('hide_feed_title option not set!')
+
         self.status = 1.0
 
         self.details_db_value_list = []
@@ -61,20 +62,20 @@ class RSSFeed(hf.module.ModuleBase):
         # Sort entries by date
         try:
             feed.entries.sort(lambda x,y: cmp(y.published_parsed,x.published_parsed))
-        except:
+        except Exception:
             pass
-        
+
         if int(self.days) != -1:
             time_diff = timedelta(days=int(self.days)).total_seconds()
             best_before_time = time()-time_diff
         else:
             best_before_time = -1
-        
+
         entries = 0
         detail_help_list = []
         for entry in feed.entries:
             details_db_values = {}
-            details_db_values['author'] = ''
+            details_db_values['author'] = entry.author
             #hide title if wanted
             if self.hide_title == 0:
                 details_db_values['title'] = 'hide_title'
@@ -84,22 +85,21 @@ class RSSFeed(hf.module.ModuleBase):
             # Convert published time to unix time integer
             try:
                 details_db_values['published'] = int(mktime(entry.published_parsed))
-            except:
+            except Exception:
                 details_db_values['published'] = 0
             details_db_values['content'] = entry.summary
             #Skip entries older than n days
             if details_db_values['published'] >= best_before_time:
                 detail_help_list.append(details_db_values)
                 entries += 1
-        
         #only show n entries
-        if int(self.entries) != '-1':
+        if int(self.entries) != -1:
             entries = int(self.entries)
-            for i in range(-int(self.entries),0):
+            for i in range(0,int(self.entries)):
                 self.details_db_value_list.append(detail_help_list[i])
         else:
             self.details_db_value_list = list(detail_help_list)
-        
+
         data['status'] = self.status
 
         return data
