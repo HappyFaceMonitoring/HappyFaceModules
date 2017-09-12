a688145db10bc5a889ef0900b4d291645881d7bc
happyface <happyface@ekp.kit.edu>
2017-09-12 12:12:56 +0200

    Storages: Add warning colours and alter database.

--- Storages.py
+++ Storages.py
@@ -24,19 +24,23 @@ import urllib2
 from ConfigParser import RawConfigParser
 
 class Storages(hf.module.ModuleBase):
-    config_keys = {'source_url': ('Source Url', '')
+    config_keys = {'source_url': ('Source Url', 'http://monitor.ekp.kit.edu/ganglia/'),
+		   'use_perc_warning': ('Lower treshold for use percentage of the disk space above which a warning is given', '80'),
+                   'use_perc_critical': ('Lower treshold for use percentage of the disk space above which the status is critical', '90')
                   }
     table_columns = [], []
     subtable_columns = {
 	'statistics' : ([
         	Column('Storage', TEXT),
-        	Column('available', TEXT),
+        	Column('used', TEXT),
         	Column('total', TEXT),
-		Column('rate', TEXT)], [])
+		Column('use_perc', TEXT)], [])
     }
 
     
     def prepareAcquisition(self):
+	# Setting defaults
+	self.source_url = self.config['source_url']
         #prepare acqusition function.
         self.logger = logging.getLogger(__name__)
         self.storages = ['ekpfs7',
@@ -47,7 +51,7 @@ class Storages(hf.module.ModuleBase):
                          'ekpfsc'
                        ]
         self.infos = ['disk_total', 'disk_free']
-        top_url = 'http://ekpmonitor.ekp.kit.edu/ganglia/'
+        top_url = 'http://monitor.ekp.kit.edu/ganglia/'
 	cfg_parser = RawConfigParser()
 	cfg_parser.read('config/ganglia.cfg')
         username = cfg_parser.get('login', 'username')
@@ -73,7 +77,7 @@ class Storages(hf.module.ModuleBase):
         for storage in self.storages:
 	    storage_dict = {'Storage': storage}
             for info in self.infos:
-                url = 'http://ekpmonitor.ekp.kit.edu/ganglia/graph.php?c=Storages&h=' \
+                url = 'http://monitor.ekp.kit.edu/ganglia/graph.php?c=Storages&h=' \
                       + storage + '.ekp.kit.edu&m=' + info + '&vl=GB&ti=Total%20Disk%20Space&json=1'
                 handle = urllib2.urlopen(url)
 		html = handle.read()
@@ -87,19 +91,30 @@ class Storages(hf.module.ModuleBase):
 			pass
 		else:
 			storage_dict[value] = 0
-	    storage_dict['available'] = float('{0:.2f}'.format(storage_dict['available']))
+	    storage_dict['used'] = float('{0:.2f}'.format(storage_dict['total'] - storage_dict['available']))
+	    del storage_dict['available']
 	    storage_dict['total'] = float('{0:.2f}'.format(storage_dict['total']))
-	    storage_dict['rate'] = int((storage_dict['total']-storage_dict['available'])/storage_dict['total']*100)
+	    storage_dict['use_perc'] = int(storage_dict['used']/storage_dict['total']*100)
 	    self.statistics_db_value_list.append(storage_dict)
+	red_use = [1 for dictionary in self.statistics_db_value_list if dictionary['use_perc'] >= int(self.config['use_perc_critical'])]
+	yellow_use = [1 for dictionary in self.statistics_db_value_list if dictionary['use_perc'] >= int(self.config['use_perc_warning'])]
+	if red_use:
+		data['status'] = 0.
+	elif yellow_use:
+		data['status'] = 0.5
+	else:
+		data['status'] = 1.
 	# Loop over all entries in the statistics list and calculate the sum.
 	sum_dict = {key: 0 for key in iter(storage_dict)}
 	sum_dict['Storage'] = 'all'
 	for dictionary in self.statistics_db_value_list:
 		for key in iter(dictionary):
-			if key != 'Storage' and key != 'rate':
+			if key != 'Storage' and key != 'use_perc':
 				sum_dict[key] += dictionary[key]
+        sum_dict['use_perc'] = int(sum_dict['used']/sum_dict['total']*100)
+	sum_dict['used'] = float('{0:.2f}'.format(sum_dict['used']))
+	sum_dict['total'] = float('{0:.2f}'.format(sum_dict['total']))
 	self.statistics_db_value_list.append(sum_dict)
-	print self.statistics_db_value_list
 	return data	
 				
 
